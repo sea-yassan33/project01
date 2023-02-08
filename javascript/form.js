a727d0a05c05dfcc371c747cff005e50396c049d
sea-yassan33 <a.yasuda_tr51@me.com>
2023-02-08 19:56:14 +0900

    V-moel_確認フォーム

--- javascript/form.js
+++ javascript/form.js
@@ -2,5 +2,11 @@ const app = new Vue({
   el: "#app",
   data: {
     testMessage: 'テスト',
+    formData: {
+      name: '',
+      email: '',
+      emailConfirm: '',
+      content: '',
+    },
   }
 })
